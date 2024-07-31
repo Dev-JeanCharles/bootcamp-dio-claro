61357845809a8cac83647832adaa1e4bb1790896
Dev-JeanCharles <jeancduasan@gmail.com>
2024-07-31 17:00:50 -0300

    feat: SmartTv

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,29 +1,15 @@
 public class Operadores {
     public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
 
-        String nomeUm = "Jean";
-        String nomeDois = new String("Jean");
-
-        System.out.println(nomeUm.equals(nomeDois));
-
-
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simOuNao = numero1 == numero2;
-
-        if(numero1 == numero2) { // true
-            System.out.println("a nossa condição é verdadeira");
+        if (condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras!");
         }
 
-        System.out.println("numeroUm é igual a numeroDois? " + simOuNao);
-
-        simOuNao = numero1 != numero2;
-
-        System.out.println("numeroUm é diferente de numeroDois? " + simOuNao);
-
-        simOuNao = numero1 > numero2;
-
-        System.out.println("numeroUm é maior que numeroDois? " + simOuNao);
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+        System.out.println("fim");
     }
 }
