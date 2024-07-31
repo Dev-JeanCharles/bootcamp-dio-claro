8d800b529fe2326ec98630e40b8cd0ea9afb3278
Dev-JeanCharles <jeancduasan@gmail.com>
2024-07-31 16:32:46 -0300

    feat: operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,11 +1,29 @@
 public class Operadores {
     public static void main(String[] args) {
-        int a, b;
 
-        a = 6;
-        b = 6;
+        String nomeUm = "Jean";
+        String nomeDois = new String("Jean");
 
-        String resultado = a==b ? "verdadeiro" : "falso";
-        System.out.println(resultado);
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simOuNao = numero1 == numero2;
+
+        if(numero1 == numero2) { // true
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simOuNao);
+
+        simOuNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente de numeroDois? " + simOuNao);
+
+        simOuNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simOuNao);
     }
 }
