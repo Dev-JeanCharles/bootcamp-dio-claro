511673c98ce59ff4fab5297b5cdfc8306b1b3ae6
Dev-JeanCharles <jeancduasan@gmail.com>
2024-07-31 16:00:59 -0300

    feat: tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,2 +1,13 @@
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
 }
