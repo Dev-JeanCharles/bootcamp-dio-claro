6bd16109296ee7e5b7c017f401944378d72f7f92
Dev-JeanCharles <jeancduasan@gmail.com>
2024-07-31 11:49:27 -0300

    feat: java basico

--- java-basico/Java-Anatomia-Classes/src/MyClass.java
+++ java-basico/Java-Anatomia-Classes/src/MyClass.java
@@ -1,5 +1,12 @@
 public class MyClass {
     public static void main (String [] args) {
-        System.out.println("Olá, Jean");
+        String primeiroNome = "Jean";
+        String segundoNome = "Charles";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
